a16cc7ae55dc367dcc7c45b17a0e3252e01d560c
Tomasz Serafin <tomasz.serafin@neontri.com>
2022-01-16 18:59:30 +0100

    Initial Commit

---
+++ TCALister/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCALister
+//
+//  Created by Tomasz Serafin on 16/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCALister/TCAListerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAListerApp.swift
+//  TCALister
+//
+//  Created by Tomasz Serafin on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TCAListerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
