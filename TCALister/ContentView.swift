2174e159bedb1c21842e09dccb89dd336e9deb03
Tomasz Serafin <tomasz.serafin@neontri.com>
2022-01-28 10:27:50 +0100

    Animal Page with async data request

--- TCALister/ContentView.swift
+++ TCALister/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            WithViewStore(self.store) { viewStore in
+                ZStack {
+                    List {
+                        ForEach(viewStore.state.items, id: \.id) { item in
+                            VStack(
+                                alignment: .leading)
+                            {
+                                Text(item.title)
+                                    .font(.system(size: 20))
+                                    .foregroundColor(.blue)
+                                if let subTitle = item.subTitle {
+                                    Text(subTitle)
+                                        .font(.system(size: 16, weight: .light))
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                        }
+                    }
+                    .navigationTitle("Animals")
+                    .onAppear { viewStore.send(.onAppear) }
+                    if viewStore.state.isLoading {
+                        ProgressView()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(
+                initialState: AppState(
+                    items:  [
+                        Animal(id: "1", title: "Item 1"),
+                        Animal(id: "2", title: "Item 2", subTitle: "Test")]),
+                reducer: appReducer,
+                environment: AppEnvironment()
+            )
+        )
+        
     }
 }
--- TCALister/TCAListerApp.swift
+++ TCALister/TCAListerApp.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCAListerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: AppState(),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                    )
+                )
         }
     }
+}
+
+struct AppState: Equatable {
+    
+    static func == (lhs: AppState, rhs: AppState) -> Bool {
+        return lhs.items == rhs.items
+    }
+    
+    var items: [Animal] = []
+    var isLoading = true
+    
+}
+
+enum AppAction {
+    case onAppear
+    case onDataReceive([Animal])
+}
+
+struct AppEnvironment {
+    
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action  {
+    case .onAppear:
+        return .future{ callback in
+            Requester.requestItems() { data in
+                callback(.success(.onDataReceive(data)))
+            }
+        }
+    case let .onDataReceive(animals):
+        state.items = animals
+        state.isLoading = false
+        return .none
+    }
+    
+    
 }
